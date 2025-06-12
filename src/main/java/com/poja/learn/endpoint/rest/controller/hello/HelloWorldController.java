736a40ed516e422009aac9df7eab88604a28ae5d
Rakotondrasoa777 <rakotondrasoatojonandrianina5@gmail.com>
2025-06-12 16:55:38 +0300

    feat: add fonctionality to upload message

--- src/main/java/com/poja/learn/endpoint/rest/controller/hello/HelloWorldController.java
+++ src/main/java/com/poja/learn/endpoint/rest/controller/hello/HelloWorldController.java
@@ -1,13 +1,23 @@
 package com.poja.learn.endpoint.rest.controller.hello;
 
+import com.poja.learn.service.HelloWorldService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class HelloWorldController {
+  private final HelloWorldService helloWorldService;
 
   @GetMapping("/hello")
   public String helloWorld() {
     return "Hello World";
+  }
+
+  @GetMapping("/uploadHello")
+  public String uploadHello(@RequestParam String name) {
+    return helloWorldService.uploadHelloWorldMessage(name);
   }
 }
