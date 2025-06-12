bb75e0d55c156cb85546a1f58874f1d1d0413bfc
Rakotondrasoa777 <rakotondrasoatojonandrianina5@gmail.com>
2025-06-12 16:34:44 +0300

    fix: rechange format

--- src/main/java/com/poja/learn/endpoint/rest/controller/hello/HelloWorldController.java
+++ src/main/java/com/poja/learn/endpoint/rest/controller/hello/HelloWorldController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
   @GetMapping("/hello")
   public String helloWorld() {
-   return "Hello World";
+    return "Hello World";
   }
 }
